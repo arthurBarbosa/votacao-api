9f28f6453acd8b84ca6d3a4ed5760ccabc77e6af
Arthur Barbosa <arthurbruno03@gmail.com>
2021-06-25 12:29:47 -0400

    Add service for save associate

--- votacao-api/votacao/src/main/java/com/arthurbarbosa/votacao/repositories/AssociateRepository.java
+++ votacao-api/votacao/src/main/java/com/arthurbarbosa/votacao/repositories/AssociateRepository.java
@@ -4,4 +4,5 @@ import com.arthurbarbosa.votacao.entities.Associate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssociateRepository extends JpaRepository<Associate, Long> {
+    boolean existsByCpf(String cpf);
 }
--- votacao-api/votacao/src/main/java/com/arthurbarbosa/votacao/services/AssociateService.java
+++ votacao-api/votacao/src/main/java/com/arthurbarbosa/votacao/services/AssociateService.java
@@ -1,4 +1,8 @@
 package com.arthurbarbosa.votacao.services;
 
+import com.arthurbarbosa.votacao.dto.AssociateRequestDTO;
+import com.arthurbarbosa.votacao.dto.AssociateResponseDTO;
+
 public interface AssociateService {
+    AssociateResponseDTO save(AssociateRequestDTO dto);
 }
