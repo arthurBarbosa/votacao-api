813828df4dd1761a3000f75e1a09411ebbffffe3
Arthur Barbosa <arthurbruno03@gmail.com>
2021-06-25 12:14:09 -0400

    Add resource for find by id schedule

--- votacao-api/votacao/src/main/java/com/arthurbarbosa/votacao/resources/ScheduleResources.java
+++ votacao-api/votacao/src/main/java/com/arthurbarbosa/votacao/resources/ScheduleResources.java
@@ -1,12 +1,10 @@
 package com.arthurbarbosa.votacao.resources;
 
 import com.arthurbarbosa.votacao.dto.ScheduleRequestDTO;
+import com.arthurbarbosa.votacao.dto.ScheduleResponseDTO;
 import com.arthurbarbosa.votacao.services.ScheduleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -24,5 +22,10 @@ public class ScheduleResources {
         var responseDTO = scheduleService.save(dto);
         var uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(responseDTO.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ScheduleResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(scheduleService.findById(id));
     }
 }
