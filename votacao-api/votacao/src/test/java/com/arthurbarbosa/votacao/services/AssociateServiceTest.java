cddd222630b4cb6d7b885540751ef8c6c5e535cf
Arthur Barbosa <arthurbruno03@gmail.com>
2021-06-26 07:56:47 -0400

    Add test unit for method find by id associate

--- votacao-api/votacao/src/test/java/com/arthurbarbosa/votacao/services/AssociateServiceTest.java
+++ votacao-api/votacao/src/test/java/com/arthurbarbosa/votacao/services/AssociateServiceTest.java
@@ -12,6 +12,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -41,4 +43,18 @@ public class AssociateServiceTest {
         assertThat(associateSave.getName()).isEqualTo("Arthur");
         assertThat(associateSave.getCpf()).isEqualTo("07390397098");
     }
+
+    @Test
+    public void should_find_associate_by_id() {
+        Long associateId = 1L;
+        var associate = Associate.builder().id(associateId).name("Arthur").cpf("07390397098").build();
+
+        Mockito.when(associateRepository.findById(associateId)).thenReturn(Optional.of(associate));
+
+        var dto = associateService.findById(associateId);
+
+        assertThat(dto.getName()).isEqualTo(associate.getName());
+        assertThat(dto.getCpf()).isEqualTo(associate.getCpf());
+    }
+
 }
