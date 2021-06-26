9f24f2c1f2213d2ab0f82638dc7d62b8eb280d55
Arthur Barbosa <arthurbruno03@gmail.com>
2021-06-26 07:50:21 -0400

    Add test unit for method save associate

--- votacao-api/votacao/src/main/java/com/arthurbarbosa/votacao/dto/AssociateResponseDTO.java
+++ votacao-api/votacao/src/main/java/com/arthurbarbosa/votacao/dto/AssociateResponseDTO.java
@@ -16,7 +16,7 @@ public class AssociateResponseDTO {
     @Schema(description = "Associate's id", example = "1")
     private Long id;
 
-    @Schema(description = "Associate's Name", example = "Fernando")
+    @Schema(description = "Associate's Name", example = "Arthur")
     private String name;
 
     @Schema(description = "Associate's CPF", example = "01373891000", minimum = "11", maximum = "11")
---
+++ votacao-api/votacao/src/test/java/com/arthurbarbosa/votacao/services/AssociateServiceTest.java
@@ -0,0 +1,44 @@
+package com.arthurbarbosa.votacao.services;
+
+import com.arthurbarbosa.votacao.dto.AssociateRequestDTO;
+import com.arthurbarbosa.votacao.entities.Associate;
+import com.arthurbarbosa.votacao.repositories.AssociateRepository;
+import com.arthurbarbosa.votacao.services.impl.AssociateServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class AssociateServiceTest {
+
+    private AssociateService associateService;
+    @MockBean
+    private AssociateRepository associateRepository;
+
+    @BeforeEach
+    public void setUp() {
+        this.associateService = new AssociateServiceImpl(associateRepository);
+    }
+
+    @Test
+    public void should_create_associate() {
+        var associate = Associate.builder().name("Arthur").cpf("07390397098").build();
+        var dto = AssociateRequestDTO.builder().name("Arthur").cpf("07390397098").build();
+        var associateSave = Associate.builder().id(1L).name("Arthur").cpf("07390397098").build();
+
+        Mockito.when(associateRepository.save(associate)).thenReturn(associateSave);
+
+        associateService.save(dto);
+
+        assertThat(associateSave.getId()).isNotNull();
+        assertThat(associateSave.getName()).isEqualTo("Arthur");
+        assertThat(associateSave.getCpf()).isEqualTo("07390397098");
+    }
+}
