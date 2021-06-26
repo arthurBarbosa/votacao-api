c31beb802f8ad35d0cae769f40a07eb4cc7d7db9
Arthur Barbosa <arthurbruno03@gmail.com>
2021-06-26 08:03:38 -0400

    Add test unit for method find all associates

--- votacao-api/votacao/src/test/java/com/arthurbarbosa/votacao/services/AssociateServiceTest.java
+++ votacao-api/votacao/src/test/java/com/arthurbarbosa/votacao/services/AssociateServiceTest.java
@@ -1,6 +1,7 @@
 package com.arthurbarbosa.votacao.services;
 
 import com.arthurbarbosa.votacao.dto.AssociateRequestDTO;
+import com.arthurbarbosa.votacao.dto.AssociateResponseDTO;
 import com.arthurbarbosa.votacao.entities.Associate;
 import com.arthurbarbosa.votacao.repositories.AssociateRepository;
 import com.arthurbarbosa.votacao.services.impl.AssociateServiceImpl;
@@ -12,6 +13,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -56,5 +60,20 @@ public class AssociateServiceTest {
         assertThat(dto.getName()).isEqualTo(associate.getName());
         assertThat(dto.getCpf()).isEqualTo(associate.getCpf());
     }
+
+    @Test
+    public void should_return_associate_list(){
+        Long associateId = 1L;
+        var associate = Associate.builder().id(associateId).name("Arthur").cpf("07390397098").build();
+        var associate1 = Associate.builder().id(1L).name("Barbosa").cpf("12345678900").build();
+
+        List<Associate> associates = new ArrayList<>(Arrays.asList(associate, associate1));
+
+        Mockito.when(associateRepository.findAll()).thenReturn(associates);
+
+        List<AssociateResponseDTO> dtos = associateService.findAll();
+        assertThat(dtos.size()).isEqualTo(2);
+    }
+
 
 }
